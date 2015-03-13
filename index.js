ae891c5668834405aa07948057444ed77926de08
Gabriel Imre <imregabriel@gmail.com>
2015-03-13 08:10:59 +0000

    added package.json

--- index.js
+++ index.js
@@ -2,18 +2,6 @@
 
 var Validator = require( './validator' );
 
-var bonafide = function ( value ) {
+exports = module.exports = function ( value ) {
     return new Validator( value );
 };
-
-Validator.addValidator( 'isString', 'not a string', function ( x ) {
-    return typeof x === 'string';
-} );
-
-var res = bonafide( 3 ).in( [3, 4] ).gt( 5 );
-
-if ( res.error ) {
-    console.log( res.error );
-}
-
-exports = module.exports;
